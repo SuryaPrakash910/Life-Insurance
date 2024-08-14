8fa99eab75ea6e6307863f4dd4b5c2238e20a349
SuryaPrakash910 <153796027+SuryaPrakash910@users.noreply.github.com>
2024-08-14 17:43:30 +0530

    Add files via upload

---
+++ Landing.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div className='landing mb-sm-5'>
+  <div className='mt-5'>
+    <h2 className='logo text-center mb-md-4' style={{ color: '#00aedf', fontWeight: '1000' }}>
+      Life Insurance Solutions
+    </h2>
+    <h1 className='motto text-center' style={{ color: '#FFA500', fontWeight: '1000' , fontSize: '30px'}}>
+      We Promise To Keep You Alright
+    </h1>
+    <p className='saying text-center mt-md-3' style={{ color: '#FFA500', fontWeight: '1000', fontSize: '30px' }}>
+      Instant everything. Great price. Big heart.
+    </p>
+  </div>
+</div>
+
+
+  );
+};
+
+export default Landing;
